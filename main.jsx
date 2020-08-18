8d8abd8788de183e5996a4104797463753545df6
hsiaoping-zhang <47858205+hsiaoping-zhang@users.noreply.github.com>
2020-08-18 10:33:49 +0800

    Add files via upload
    main file upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+window.userName = "none";
+console.log("restart")
+ReactDOM.render( <App/> , document.getElementById('root'));
