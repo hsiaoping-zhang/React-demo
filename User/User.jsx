a6db94e85b8b4d149732e2c9259eb89ef0dfc616
hsiaoping.zhang <e44066034@gs.ncku.edu.tw>
2020-08-18 11:02:28 +0800

    remove redundant folder

--- User/User.jsx
+++
@@ -1,2 +0,0 @@
-window.userName = "tester"
-export {userName};
--- login.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Login=()=>{
-    return(
-            <h1 style={{color:"white",fontFamily:"Microsoft JhengHei"}}>我是第一頁</h1>
-    )
-}
-
-export default Login;
